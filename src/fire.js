5e1a5f14a95c405949eb0b2c6ebf4b134aed8d26
maltinho8 <malte.raeuchle@gmx.de>
2022-03-14 17:06:02 +0100

    BB: Fix Bugs with firebase

--- src/fire.js
+++ src/fire.js
@@ -7,8 +7,22 @@
 
 import firebase from 'firebase';
 
+const obj = JSON.parse('{"name":"John", "age":30, "city":"New York"}');
+console.log(obj);
 
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+
+};
+
+
+console.log (firebaseConfig);
 
 try {
   firebase.initializeApp(firebaseConfig);
