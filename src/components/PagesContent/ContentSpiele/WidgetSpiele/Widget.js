c18fa825eb88603f88f4462c156afa2b1982d31b
maltinho8 <malte.raeuchle@gmx.de>
2022-04-12 23:48:15 +0200

    BB: 2nd try Widgets

--- src/components/PagesContent/ContentSpiele/WidgetSpiele/Widget.js
+++ src/components/PagesContent/ContentSpiele/WidgetSpiele/Widget.js
@@ -6,7 +6,12 @@ import './Widget.css';
 const WidgetTabelle = () => {
     return (
         <>
-     
+     <div id="widget3"></div>
+     <Helmet>
+	<script type="text/javascript">
+		new fussballdeWidgetAPI().showWidget('widget3', '02HRA8MP9K000000VUM1DNP5VVSHME5T');
+	</script>
+  </Helmet>
       </>
 
     );
--- src/components/PagesContent/ContentSpielplan/WidgetSpielplan/Widget.js
+++ src/components/PagesContent/ContentSpielplan/WidgetSpielplan/Widget.js
@@ -6,7 +6,12 @@ import './Widget.js';
 const WidgetSpielplan= () => {
     return (
         <>
-     
+      <div id="widget2"></div>
+      <Helmet>
+	<script type="text/javascript">
+		new fussballdeWidgetAPI().showWidget('widget2', '02HRA8A8Q0000000VUM1DNPIVTBOE0VE');
+	</script>
+  </Helmet>
       </>
 
     );
