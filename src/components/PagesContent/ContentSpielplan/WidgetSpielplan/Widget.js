63a4c3e05dabbf36f03adffc263b83bec77dfec0
maltinho8 <malte.raeuchle@gmx.de>
2022-04-13 10:46:29 +0200

    BB: Deploy test with important!

--- src/components/PagesContent/ContentSpielplan/WidgetSpielplan/Widget.js
+++ src/components/PagesContent/ContentSpielplan/WidgetSpielplan/Widget.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {Helmet} from 'react-helmet';
-import './Widget.js';
+import './Widget.css';
 
 
 const WidgetSpielplan= () => {
     return (
         <>
-      <div id="widget1"></div>
+      <div id="widget2"></div>
       <Helmet>
 	<script type="text/javascript">
-		new fussballdeWidgetAPI().showWidget('widget1', '02HRA8A8Q0000000VUM1DNPIVTBOE0VE');
+		new fussballdeWidgetAPI().showWidget('widget2', '02HRA8A8Q0000000VUM1DNPIVTBOE0VE');
 	</script>
   </Helmet>
       </>
